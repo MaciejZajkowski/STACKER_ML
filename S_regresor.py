b7a437a0c2401131dfb5306a2aa54dcec78fc383
Zajkos1 <mzx2000@gmail.com>
2022-05-10 22:35:00 +0200

    m

--- S_regresor.py
+++ S_regresor.py
@@ -107,7 +107,7 @@ class Stacker_Regresion:
         f_pred,pred = Stacker_Regresion.predict(self = self,X=X)
         print("\nEvaluating Final model: \n")
         Stacker_Regresion.evaluate(self=self,Y=Y,pred=f_pred)
-        for i in range(len(pred)+1):
-            print("\nEvaluating model: {} \n".format(Stacker_Regresion.models[i]))
+        for i in range(len(pred)):
+            print("\nEvaluating model: {} \n".format(Stacker_Regresion.models[i+1]))
             Stacker_Regresion.evaluate(self = self,Y=Y,pred = pred[i])
         
