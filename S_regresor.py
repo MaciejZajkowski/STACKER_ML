25cab2d112e963440a0fa724a1bc27ba9e33a23a
Zajkos1 <mzx2000@gmail.com>
2022-05-11 11:10:24 +0200

    Update S_regresor.py

--- S_regresor.py
+++ S_regresor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sklearn as skl
 from sklearn.model_selection import train_test_split
+from random import randint
 
 class Stacker_Regresion:
     """ 
@@ -13,6 +14,7 @@ class Stacker_Regresion:
         Stacker_Regresion.models = models
         Stacker_Regresion.Classfier = Classifier
         Stacker_Regresion.training_sets_split = training_sets_split
+        Stacker_Regresion.Trained_models = [False for i in range(len(models))]
         
     def get_clousest_clases(self,X,Y):
         """
@@ -23,13 +25,15 @@ class Stacker_Regresion:
         
         err  =[]
         predictions = []
+        cnt = 0
         for model in Stacker_Regresion.models:
-            pred = model.predict(X)
-            if(pred.shape[0] != Y.shape):
-                pred = pred.reshape(1,-1)
-            predictions.append(pred.squeeze())
-            err.append((np.square( pred**2 - Y **2)).squeeze())
-            
+            if Stacker_Regresion.Trained_models[cnt]:
+                pred = model.predict(X)
+                if(pred.shape[0] != Y.shape):
+                    pred = pred.reshape(1,-1)
+                predictions.append(pred.squeeze())
+                err.append((np.square( pred**2 - Y **2)).squeeze())
+            cnt +=1
         classes = []
         for i in range(len(err[0])):
             temp = []
@@ -39,22 +43,35 @@ class Stacker_Regresion:
             
         return pd.DataFrame(classes,columns=['Best_model']), pd.DataFrame(predictions)
     
-    def fit(self,X,Y):
+    def fit(self,X,Y,models_to_train = [],random_state = randint(1,100)):
         """
             separates data then fits models, and clasifier to chouse what data is the best 
             returns table of history (if you are using keras, in skl models returns just model names so use it wisely)
             
+            models_to_train requaiers list of boolian type where place in list coresponds to each model, you need to pass true if you wan to train model
+            training every model is deafoult, it fills extra empty spots by True, so if you want to train all models exept first you can do it 
+            
+            random state is used to train test split, it is important to use your own random state when training models separetly - if not it may cause mixing
+            datasets for difrent lines of model - it will cause clasifier to malfunction -  
+            
+            
         """
-        
+        if len(models_to_train) != len(Stacker_Regresion.models) and len(models_to_train) < len(Stacker_Regresion.models):
+            for i in range( len(Stacker_Regresion.models) -len( models_to_train) ):
+                models_to_train.append(True)
         from IPython.display import clear_output
         " Line 1 of model -  "
-        X_train1, X_train2, Y_train1, Y_train2 = train_test_split(X,Y, test_size=Stacker_Regresion.training_sets_split)
-        
+        X_train1, X_train2, Y_train1, Y_train2 = train_test_split(X,Y, test_size=Stacker_Regresion.training_sets_split,random_state=random_state)
         hist = []
+        #fiting models
+        cnt = 0
         for model in Stacker_Regresion.models:
             clear_output(wait=True)
-            print("training model, name: {}".format(model))
-            hist.append(model.fit(X_train1,Y_train1))
+            if models_to_train[cnt]:
+                print("training model, name: {}".format(model))
+                hist.append(model.fit(X_train1,Y_train1))
+                Stacker_Regresion.Trained_models[cnt] = True
+            cnt += 1
         
         classes,pred = Stacker_Regresion.get_clousest_clases(self,X_train2,Y_train2)
         
@@ -64,12 +81,14 @@ class Stacker_Regresion:
         
     def predict(self,X):
         predictions = []
-        
+        cnt = 0
         for model in Stacker_Regresion.models:
-            pred = model.predict(X)
-            if(pred.shape[0] != X.shape[0]):
-                pred = pred.reshape(1,-1)
-            predictions.append(pred.squeeze())
+            if Stacker_Regresion.Trained_models[cnt]:
+                pred = model.predict(X)
+                if(pred.shape[0] != X.shape[0]):
+                    pred = pred.reshape(1,-1)
+                predictions.append(pred.squeeze())
+            cnt+=1
             
             
         temp = Stacker_Regresion.Classfier.predict(X)
@@ -103,11 +122,17 @@ class Stacker_Regresion:
         return x
     
     def evaluate_models(self,X,Y):
-        
+        """evaluating every trained model and final stacker
+
+        Args:
+            X (_type_): _description_
+            Y (_type_): _description_
+        """
         f_pred,pred = Stacker_Regresion.predict(self = self,X=X)
         print("\nEvaluating Final model: \n")
         Stacker_Regresion.evaluate(self=self,Y=Y,pred=f_pred)
-        for i in range(len(pred)):
-            print("\nEvaluating model: {} \n".format(Stacker_Regresion.models[i]))
-            Stacker_Regresion.evaluate(self = self,Y=Y,pred = pred[i])
+        for i in range(Stacker_Regresion.models):
+            if Stacker_Regresion.Trained_models[i]:
+                print("\nEvaluating model: {} \n".format(Stacker_Regresion.models[i]))
+                Stacker_Regresion.evaluate(self = self,Y=Y,pred = pred[i])
         
