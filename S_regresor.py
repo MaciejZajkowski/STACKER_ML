d579cde9da8182664acd9722f0b2f274fbc56d92
Zajkos1 <mzx2000@gmail.com>
2022-05-11 14:09:47 +0200

    Update S_regresor.py

--- S_regresor.py
+++ S_regresor.py
@@ -78,7 +78,7 @@ class Stacker_Regresion:
         classes,pred = Stacker_Regresion.get_clousest_clases(self,X_train2,Y_train2)
         
         if Stacker_Regresion.Boosting:
-            F_set = pd.concat([pd.DataFrame( X_train2) ,pd.DataFrame(pred)],axis=1,join='inner').values
+            F_set = pd.concat([pd.DataFrame( X_train2) ,pd.DataFrame(pred)],axis=1,join='inner').values.reshape(-1,1)
             hist.append( Stacker_Regresion.Final_model.fit(F_set,Y_train2))
         else:
             hist.append( Stacker_Regresion.Final_model.fit(X_train2,classes))
